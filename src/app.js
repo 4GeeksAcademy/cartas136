bf333c002c7a493812e7df08e99982a901f5452c
Lucas Ezequiel Hernandez <lucasezequielhernandez@gmail.com>
2026-04-19 17:06:36 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+    const suits = ['Corazones', 'Diamantes', 'Picas', 'Tréboles'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    const card = document.getElementById('card');
+    
+    card.className = (randomSuit === 'Corazones' || randomSuit === 'Diamantes') ? 'card red' : 'card black';
+    
+    document.getElementById('suit-top').innerText = randomSuit;
+    document.getElementById('suit-bottom').innerText = randomSuit;
+    document.getElementById('value').innerText = randomValue;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById('button').addEventListener('click', generateCard);
+window.onload = generateCard;
